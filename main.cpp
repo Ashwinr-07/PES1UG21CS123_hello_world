6bc743c2dbb4f7bb7a58459fd230e2c7de008add
Ashwin Ragupathy <105007681+Ashwinr-07@users.noreply.github.com>
2024-03-09 23:45:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
-    std::cout << "Hello, world! This is PES1UG21CS096" << std::endl;
+    std::cout << "Hello, world! This is PES1UG21CS123" << std::endl;
     return 0;
 }
